02ba6f62085960566408ece6ce5547e7597ea492
gityBoii <gnewid88@gmail.com>
2022-09-05 16:38:15 +0900

    55

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 
 int main() //메인함수. 입력값
 {
-	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << std::endl; // 문장 출력
+	//std = standard / cout = character output
+
+	std::cout << "두번째 문장" << std::endl;
 
 	return 0; //반환값. 0
+
 }
